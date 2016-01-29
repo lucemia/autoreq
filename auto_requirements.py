b06ae4d98056a57d7210ba97144020f64e6bc822
lucemia <lucemia@gmail.com>
2016-01-29 15:20:13 +0800

    fix requirements, finish features

--- auto_requirements.py
+++ auto_requirements.py
@@ -2,6 +2,11 @@ import pandas
 from itertools import izip
 import os
 import cPickle
+import re
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
 
 url = "https://pypi.python.org/pypi?%3Aaction=index"
 package_url = 'https://pypi.python.org/pypi/%s'
@@ -64,34 +69,94 @@ def _is_desp(line):
 
 index = _build_index()
 
+package_url = 'https://pypi.python.org/pypi/%s/%s'
 
-def fix(filepath):
-    with open(filepath) as ifile:
-        lines = [iline.strip() for iline in ifile]
 
+def _parse_package_page(content):
+    soup = BeautifulSoup(content)
+    title = soup.select('.section')[0].select('h1')[0].text
+    desp = soup.select(".section")[0].select('p')[0].text
+
+    package, version = title.split()
+    return package, version, desp
+
+
+def _parse_index_page(content):
+    import cStringIO
+    assert '<title>Index of Packages : Python Package Index</title>' in content
+
+    df = pandas.io.html.read_html(cStringIO.StringIO(content), header=0)
+    return df[0]
+
+
+def _parse_requirment(content):
+    lines = content.split('\n')
     packages = []
+    FORMAT = re.compile(r"([\w\-\_]+)((>=|==)([\.\d]+))?")
+
     for line in lines:
+        line = line.strip()
+        if not line:
+            continue
         if line[0] == '-':
-            packages.append(line)
+            packages.append({
+                'type': 'ext',
+                'line': line
+            })
 
         elif _is_package(line) and line[0] != '#':
-            packages.append(line)
+            package, _, condition, version = FORMAT.findall(line)[0]
+            packages.append({
+                'type': 'package',
+                'package': package,
+                'condition': condition,
+                'version': version,
+                'line': line
+            })
+
+    return packages
+
+
+def fix(filepath):
+    packages = _parse_requirment(open(filepath).read())
+
+    results = []
+    PACKAGE_URL = 'https://pypi.python.org/pypi/%s/%s'
+
+    for package_info in packages:
+        if package_info['type'] != 'package':
+            results.append(package_info['line'])
+            continue
+
+        package = package_info['package']
+        condition = package_info['condition']
+        version = package_info['version']
+
+        package_url = PACKAGE_URL % (package, version)
+
+        print 'process', package, package_url
+        resp = requests.get(package_url)
+
+        if '<title>Index of Packages : Python Package Index</title>' in resp.content:
+            # it is index page
+            df = _parse_index_page(resp.content)
+            title = df["Package"][0]
+            package, version = title.split()
+
+            resp = requests.get(PACKAGE_URL % (package, version))
+
+        package, version, desp = _parse_package_page(resp.content)
+        results.append("# %s" % desp)
+        results.append("# %s" % (PACKAGE_URL % (package, version)))
+        if condition:
+            results.append("%s%s%s" % (package, condition, version))
+        else:
+            results.append("%s==%s" % (package, version))
+
+        results.append('\n')
 
     with open(filepath, 'w') as ofile:
-        for package in packages:
-            if package[0] == '-':
-                ofile.write(package)
-                ofile.write('\n')
-
-            package = package.split('=')[0]
-
-            x = index[package]
-
-            ofile.write('# %s\n' % x["desp"])
-            ofile.write("# %s\n" % (package_url % package))
-            ofile.write(package + "\n")
-            ofile.write("\n")
-
+        ofile.write('\n'.join(results))
 
 if __name__ == "__main__":
-    import clime.now
+    import clime; clime.start(debug=True)
